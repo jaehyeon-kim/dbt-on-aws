1bc783ac6428fe6a10f49cdd8f8597dceacbdf6e
Jaehyeon Kim <dottami@gmail.com>
2022-09-30 09:01:57 +1000

    simplify stack

--- glue/infra/providers.tf
+++ glue/infra/providers.tf
@@ -6,10 +6,5 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 3.72"
     }
-
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.0.1"
-    }
   }
 }
--- glue/infra/variables.tf
+++ glue/infra/variables.tf
@@ -1,3 +1,9 @@
+# Find the user currently in use by AWS
+data "aws_caller_identity" "current" {}
+
+# Region in which to deploy the solution
+data "aws_region" "current" {}
+
 locals {
   name        = basename(path.cwd) == "infra" ? basename(dirname(path.cwd)) : basename(path.cwd)
   region      = data.aws_region.current.name
